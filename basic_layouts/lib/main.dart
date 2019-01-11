70c03e7dc0d463a8290e69c481f4c5124a5867ac
lixuanqi <lixuanqi1995@gmail.com>
2019-01-10 22:55:50 -0800

    Step 3: Implement the button row

--- basic_layouts/lib/main.dart
+++ basic_layouts/lib/main.dart
@@ -3,44 +3,69 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32.0),
-    child: Row(
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Text(
-                  'Oeschinen Lake Campground',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Text(
-                'Kandersteg Switzerland',
-                style: TextStyle(
-                  color: Colors.grey[500]
-                ),
-              ),
-            ],
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8.0),
+          child: Text(
+            label,
+            style: TextStyle(
+                fontSize: 12.0, fontWeight: FontWeight.w400, color: color),
           ),
         ),
-        Icon(
-          Icons.star,
-          color:Colors.red[500]
-        ),
-        Text('41'),
       ],
-    ),
-  );
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg Switzerland',
+                  style: TextStyle(color: Colors.grey[500]),
+                ),
+              ],
+            ),
+          ),
+          Icon(Icons.star, color: Colors.red[500]),
+          Text('41'),
+        ],
+      ),
+    );
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter Layout Demo',
       home: Scaffold(
@@ -50,6 +75,7 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: <Widget>[
             titleSection,
+            buttonSection,
           ],
         ),
       ),
